bccd270efccb306a69445cce2a3acf7e2facced5
Simen Romstad <simen.romstad@bouvet.no>
2022-05-30 16:24:15 +0200

    Add button to status bar

--- extension.js
+++ extension.js
@@ -1,17 +1,33 @@
 const vscode = require('vscode');
-
+//const {window, StatusBarAlignment} from 'vscode';
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('"SAD" is now active!');
-	
 	let dispB = vscode.commands.registerCommand("SAD.sad", function() {
-		vscode.debug.stopDebugging();
+        vscode.debug.stopDebugging();
 		vscode.window.showInformationMessage('All running debuggers stopped');
-		})
+        buttonInstance.hide();
+    })
 	context.subscriptions.push(dispB);
+    
+    
+    let buttonInstance = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+    buttonInstance.text = `$(debug-stop) Stop all debugging`;
+    buttonInstance.command = "SAD.sad";
+    buttonInstance.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground');
+    context.subscriptions.push(buttonInstance);
+    
+    vscode.tasks.onDidStartTask(()=>{
+        if(vscode.workspace.getConfiguration('SAD').get("showButtonOnTaskEvent")) {
+            buttonInstance.show();
+        }
+    });
+    vscode.debug.onDidStartDebugSession(()=>{
+        buttonInstance.show();
+    });
 }
+
 
 function deactivate() {}
 
